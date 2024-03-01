7397d9656d2116b8f1f21491edcdfc7d53846018
Gaurav Sharma <gsharma1154@gmail.com>
2024-03-02 00:57:19 +0530

    removed config class

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Aarav Kumar', 'aarav.kumar@example.com', '123 MG Road, Delhi', '987-654-3210', '10A', 'Rajesh Kumar');
-INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Diya Sharma', 'diya.sharma@example.com', '456 Lajpat Nagar, Mumbai', '876-543-2109', '10B', 'Manoj Sharma');
-INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Rohan Gupta', 'rohan.gupta@example.com', '789 Gandhi Marg, Kolkata', '765-432-1098', '11A', 'Suresh Gupta');
-INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Isha Patel', 'isha.patel@example.com', '1011 Nehru Place, Ahmedabad', '654-321-0987', '11B', 'Amit Patel');
-INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Aditya Singh', 'aditya.singh@example.com', '1213 Malviya Nagar, Bangalore', '543-210-9876', '12A', 'Vikram Singh');
+INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Aarav Kumar', 'aarav.kumar@example.com', '123 MG Road, Delhi', '987-65-3210', '10A', 'Rajesh Kumar');
+INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Diya Sharma', 'diya.sharma@example.com', '456 Lajpat Nagar, Mumbai', '876-54-2109', '10B', 'Manoj Sharma');
+INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Rohan Gupta', 'rohan.gupta@example.com', '789 Gandhi Marg, Kolkata', '765-43-1098', '11A', 'Suresh Gupta');
+INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Isha Patel', 'isha.patel@example.com', '1011 Nehru Place, Ahmedabad', '654-32-0987', '11B', 'Amit Patel');
+INSERT INTO Student (name, email, address, phone_number, class_name, fathers_name) VALUES ('Aditya Singh', 'aditya.singh@example.com', '1213 Malviya Nagar, Bangalore', '543-21-9876', '12A', 'Vikram Singh');
